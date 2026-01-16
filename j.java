9e21a086d7aa2d79d9c50faf595de9b986fce51b
anu <anwarseifu6@gmail.com>
2026-01-16 21:56:14 +0300

    modified

--- j.java
+++ j.java
@@ -1,6 +1,13 @@
 public class j {
+
+    public int add(int num1, int num2){
+        return num1 + num1;
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+        j j1 = new j();
+        j1.add(2, 4);
+
     }
 }
